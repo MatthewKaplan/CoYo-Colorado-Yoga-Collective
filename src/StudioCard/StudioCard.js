4557d1208afaf92f5f5c8a2deb77a63b7edc3b34
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-04-04 08:42:55 -0600

    Added stars to represent each studios google rating

--- src/StudioCard/StudioCard.js
+++ src/StudioCard/StudioCard.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './StudioCard.css';
 
 
-
 const studioCardUI = ( props ) => {
+  const starsInnerStyle = {
+  width: props.roundedRating
+};
+
   return (
     <div className="cardContainer">
       <section className="studioCards">
@@ -11,10 +14,21 @@ const studioCardUI = ( props ) => {
         <p className="studioName">{props.name}</p>
         <p className="studioAddress">{props.address}</p>
         <p className="studioNumber">{props.phone}</p>
-        <p className="studioMission">{props.mission}</p>
-        <p className="studioRating">{props.rating}</p>
+        <div className="mission-container">
+          <h4 className="mission-heading">Mission:</h4>
+          <p className="studioMission">{props.mission}</p>
+        </div>
+        <div className="inline">
+          <div className="starsOuter">
+            <div className="starsInner" style={starsInnerStyle}>
+            </div>
+          </div>
+          <p className="studioRating">{props.rating}</p>
+        </div>
         <a href={props.link} className="studioLink" target="_blank">{props.name} Website</a>
-        <p className="studioFee">{props.fee}</p>
+        <div className="studioFee">{props.fee} 
+          <p className="drop-in-text">drop in</p> 
+        </div>
       </section>
     </div>
   )
