b12cb841101fd70eafe2a306aa45014bb97de910
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-04-03 22:57:51 -0600

    Added search functionality to the page so that you can search the stuidos by name

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,1 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Search.css";
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: ''
+    }
+  }
+
+  handleInputChange = () => {
+    this.setState({
+      searchQuery: this.search.value
+    }, this.props.updateResults(this.state.searchQuery));
+  }
+
+  render() {
+    return (
+      <section className="searchStudioContainer">
+        <form className="searchStudio">
+          <input 
+            placeholder="Search By Studio Name"
+            ref={input => this.search = input} 
+            onChange={this.handleInputChange}
+            className="studioSearch" 
+          />
+        </form>
+      </section>
+    );
+  }
+}
+
+export default Search;
