8282f5c4178b503ce113bb497eef4b17424434e7
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-04-05 11:16:21 -0600

    displaying yoga poses on new line
    Co-authored-by: Sally Haefling <sallyhaefling@gmail.com>
    Co-authored-by: Kelly Zick <Kllyzck@gmail.com>

--- src/typeCard/TypeCard.js
+++ src/typeCard/TypeCard.js
@@ -5,9 +5,12 @@ const TypeCard = ( props ) => {
   return (
     <section className="mainContainer">
       <h4 className='typeHeading'>{props.name}</h4>
-      <h5 className='purpose'>Purpose: {props.purpose}</h5>
-      <p className='beginner'>Difficulty: {props.beginnerFriendly ? 'Beginner friendly' : 'Advanced'}</p>
-      <p className='poses'>Poses: {props.commonPoses}</p>
+      <h5 className="purpose">Purpose:</h5>
+      <h5 className='purpose'>{props.purpose}</h5>
+      <p className="beginner">Difficulty:</p>
+      <p className='beginner'>{props.beginnerFriendly ? 'Beginner friendly' : 'Advanced'}</p>
+      <p className="poses">Poses: </p>
+      <p className='poses'>{props.commonPoses}</p>
     </section>
   )
 }
