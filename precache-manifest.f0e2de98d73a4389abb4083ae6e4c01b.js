b9b92e80c009d290e0f03e829a87e182fda582b0
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-22 10:52:32 -0600

    Updates

---
+++ precache-manifest.f0e2de98d73a4389abb4083ae6e4c01b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "bda65ee0d3f5d9a780bb",
+    "url": "/COYO/static/css/main.ba9ef16b.chunk.css"
+  },
+  {
+    "revision": "bda65ee0d3f5d9a780bb",
+    "url": "/COYO/static/js/main.98aeaee5.chunk.js"
+  },
+  {
+    "revision": "7e1f9f7a0bd226ee87bf",
+    "url": "/COYO/static/js/runtime~main.703a5dd3.js"
+  },
+  {
+    "revision": "5c721eb88b16fd7080d5",
+    "url": "/COYO/static/js/2.b9724783.chunk.js"
+  },
+  {
+    "revision": "fea41fc3bb84c8a31f03afb39fa099d6",
+    "url": "/COYO/static/media/colorado-mountains.fea41fc3.jpg"
+  },
+  {
+    "revision": "c50acbd986bedf1b79a9552b838b8aa3",
+    "url": "/COYO/static/media/logo.c50acbd9.png"
+  },
+  {
+    "revision": "1d0f525fcfad435d3b4885f63f9d9c82",
+    "url": "/COYO/static/media/ANUSARA.1d0f525f.png"
+  },
+  {
+    "revision": "2698a632c4594e6338ce390c8c56cb4e",
+    "url": "/COYO/static/media/ASHTANGA.2698a632.png"
+  },
+  {
+    "revision": "356ee4cbaa9b3229d1aed241281960fc",
+    "url": "/COYO/static/media/BIKRAM.356ee4cb.png"
+  },
+  {
+    "revision": "2da4b764e53b40c9cbdb5e768405ced4",
+    "url": "/COYO/static/media/HATHA.2da4b764.png"
+  },
+  {
+    "revision": "8f6164b0912faca54b6a655a421e0073",
+    "url": "/COYO/static/media/IYENGAR.8f6164b0.png"
+  },
+  {
+    "revision": "41aa647996a324c36625f644c5f4d8e1",
+    "url": "/COYO/static/media/JIVAMUKTI.41aa6479.png"
+  },
+  {
+    "revision": "28604c74700256660d010e359cdec05f",
+    "url": "/COYO/static/media/KUNDALINI.28604c74.png"
+  },
+  {
+    "revision": "b04f38d76eb70c4b5ca4c27b662410eb",
+    "url": "/COYO/static/media/VINYASA.b04f38d7.png"
+  },
+  {
+    "revision": "482816d64f4c26df64dbb5040e3f6fd5",
+    "url": "/COYO/static/media/YIN.482816d6.png"
+  },
+  {
+    "revision": "97221bbf890c1748b5eab5bbb7913828",
+    "url": "/COYO/static/media/RESTORATIVE.97221bbf.png"
+  },
+  {
+    "revision": "1e413d965fe30adf670ef8d3a032ddfa",
+    "url": "/COYO/index.html"
+  }
+];
