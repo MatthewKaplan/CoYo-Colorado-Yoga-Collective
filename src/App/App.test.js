4f68e777b3a0577cc8fdb6902876f84955767615
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-22 11:00:07 -0600

    Clean up files and deploy application

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,79 +1,20 @@
 import React from 'react';
 import App from './App';
 import { shallow } from 'enzyme';
+import MockData from "../Helper/mockData";
+
+let mockYogaTypes = MockData.mockTypes;
+let mockYogaStudios = MockData.mockStudios;
 
 describe('App', () => {
-
-  let mockTypes = [
-    {
-    name: "Hatha",
-    typeId: 1,
-    propsUsed: false,
-    commonPoses: [
-    "Vrikshasana",
-    "Salabhasana",
-    "Tadasana",
-    "Uttanasana",
-    "Adho Mukha Svanasana",
-    "Setu Bandhasana"
-    ],
-    beginnerFriendly: true,
-    purpose: "To align and calm your body, mind, and spirit in preparation for meditation."
-    },
-    {
-    name: "Iyengar",
-    typeId: 2,
-    propsUsed: true,
-    commonPoses: [
-    "Tadasana",
-    "Paripurna Navasana",
-    "Adho Mukha Virasana",
-    "Ustrasana",
-    "Utthita Marichyasana",
-    "Gomukhasana"
-    ],
-    beginnerFriendly: true,
-    purpose: "To improve strength, awareness, mobility and stability."
-    }]
-
-  let mockStudios = [
-    {
-    name: "Whole Yoga",
-    mainTypeOffered: 5,
-    address: "1735 E 17th Ave, Denver, CO 80218",
-    phoneNum: "(303)333-9642",
-    openEveryday: true,
-    mission: "Where body and mind become whole.",
-    googleRating: 4.8,
-    dropInFee: "$17.00",
-    teacherTraining: true,
-    link: "https://wholeyoga.com",
-    img: "https://i.imgur.com/w7KfQWv.jpg"
-    },
-    {
-    name: "Samadhi Yoga",
-    mainTypeOffered: 5,
-    address: "639 E 19th Ave, Denver, CO 80203",
-    phoneNum: "(303)860-9642",
-    openEveryday: true,
-    mission: "To honor the science of yoga as a methodical process to create a lifestyle of health, happiness, and abundance.",
-    googleRating: 4.7,
-    dropInFee: "$20.00",
-    teacherTraining: true,
-    link: "https://samadhiyoga.net",
-    img: "https://i.imgur.com/fkUvUxr.jpg"
-    }]
 
   let mockMessage = 'This is a mock message';
 
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(
-      <App 
-      studios={mockStudios}
-      yogaTypes={mockTypes}
-      />
-    )
+      <App studios={mockYogaStudios} yogaTypes={mockYogaTypes} />
+    );
   });
 
   it('should render without errors', () => {
@@ -103,13 +44,13 @@ describe('App', () => {
 
   it('should store rendered studios', () => {
     expect(wrapper.state('rendered')).toEqual( [] );
-    wrapper.instance().storeRendered(mockStudios);
+    wrapper.instance().storeRendered(mockYogaStudios);
     expect(wrapper.state('rendered')).toHaveLength(2);
   });
 
   it('should update yoga types', () => {
     expect(wrapper.state('yogaTypes')).toEqual( [] );
-    wrapper.instance().updateTypes(mockTypes);
+    wrapper.instance().updateTypes(mockYogaTypes);
     expect(wrapper.state('yogaTypes')).toHaveLength(2);
   });
 
